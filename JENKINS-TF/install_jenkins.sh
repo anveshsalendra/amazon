085e3d6a05683ea9072e84196ef7c46f6510b292
anveshsalendra <anveshsalendra1999@gmail.com>
2025-10-19 09:58:52 +0530

    Update install_jenkins.sh

--- JENKINS-TF/install_jenkins.sh
+++ JENKINS-TF/install_jenkins.sh
@@ -29,6 +29,7 @@ sudo apt-get install trivy -y
 
 
 
+
 # install terraform
 sudo apt install wget -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -43,3 +44,10 @@ sudo apt install curl -y
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
+
+
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
